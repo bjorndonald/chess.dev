5fff5ec59a3634283794958f0a14f0919655c955
bjorndonald <bjorndonaldb@yahoo.com>
2024-07-14 20:48:23 +0100

    fix: check

--- src/drizzle/0000_eager_runaways.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "games" (
-	"id" text PRIMARY KEY NOT NULL,
-	"type" text,
-	"pgnString" text,
-	"white" numeric,
-	"black" numeric,
-	"created_at" timestamp,
-	"updated_at" timestamp
-);
--- src/drizzle/0001_curved_red_hulk.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "games" ALTER COLUMN "white" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "games" ALTER COLUMN "black" SET DATA TYPE text;
